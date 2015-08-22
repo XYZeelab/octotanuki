8918692d868fd676dc2ef0968ae87ccc555806e6
Anthony <enzo.cpc@Gmail.com>
2015-08-22 10:12:25 +0800

    More fun time

--- main.cpp
+++ main.cpp
@@ -1,60 +1,100 @@
 /*
- * main.cpp
- *
- *  Created on: Aug 18, 2015
- *      Author: Anthony
+  Web Server
+
+ A simple web server that shows the value of the analog input pins.
+ using an Arduino Wiznet Ethernet shield.
+
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ * Analog inputs attached to pins A0 through A5 (optional)
+
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+
  */
 
+#include <SPI.h>
+#include "Ethernet.h"
 
-#include "arduino.h"
-#include "Wire.h"
-
-const int SLAVE_ADDRESS(0x40);
-
-enum DataFrameFlags {
-	START_FLAG=0x12,
-	END_FLAG=0x13,
-	ESC_FLAG=0x7D
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
+IPAddress ip(192, 168, 1, 177);
 
-void receiveEvent (int size);
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
 
-void setup(void)
-{
-    pinMode(13, OUTPUT);
-    Wire.begin(SLAVE_ADDRESS);
-    Wire.onReceive(receiveEvent);
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
 }
 
-void loop(void)
-{
-    digitalWrite(13, HIGH);
-    delay(200);
-    digitalWrite(13, LOW);
-    delay(2500);
-}
 
-void receiveEvent (int size)
-{
-	uint8_t rBuffer[BUFFER_LENGTH];
-	bool hasStartFlag = false;
-	bool hasEndFlag = false;
-	int idx = 0;
-
-	if (Wire.available() > 1)
-	{
-		uint8_t ch = Wire.read();
-
-		// Determine if the first byte is the start flag.
-		if (ch == START_FLAG) {
-			hasStartFlag = true;
-			while (Wire.available() > 1)
-			{
-				rBuffer[idx] = Wire.read();
-				idx++;
-			}
-		} else {
-			// Drop frame.
-		}
-	}
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
 }
